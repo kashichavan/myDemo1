1fb35a75c8ec215c00f5dbce4692f69dc6e1fff8
kashichavan <kashicahavn7777@gmail.com>
2024-12-29 23:04:57 +0530

    v1

--- myDemo1/settings.py
+++ myDemo1/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.contrib.sessions.middleware.SessionMiddleware',
+    
     'django.middleware.security.SecurityMiddleware',
     'corsheaders.middleware.CorsMiddleware'
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
