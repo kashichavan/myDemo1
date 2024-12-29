1326a30aee1330e6e97e407467fbd608ebc6028b
kashichavan <kashicahavn7777@gmail.com>
2024-12-29 23:01:07 +0530

    n1

---
+++ myapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .froms import *
+# Create your views here.
+def create(request):
+    if request.method=='POST':
+        st=StudentForm(request.POST)
+        if st.is_valid():
+            st.save()
+    st=StudentForm()
+    return render(request,'create.html',st)
+
+
+def show(request):
+    data=Student.objects.all()
+    return render(request,'show.html',{'data':data})
